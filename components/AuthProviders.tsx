0161773a297cfdac243cb8ca40629abf6037c85a
Dinesh P <dineshp@Dineshs-MacBook-Air.local>
2023-07-08 17:16:23 +0530

    added-auth

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+"use client";
+
+import { getProviders, signIn } from 'next-auth/react'
+import React, { useState, useEffect } from 'react';
+import Button from './Button';
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | null;
+
+}
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
+  const [Providers, setProviders] = useState<Providers | null>(null);
+
+useEffect(() => {
+  const fetchProviders = async () => {
+    const res = await getProviders();
+    setProviders(res);
+  }
+  fetchProviders();
+},[])
+
+ if(Providers){
   return (
-    <div>AuthProviders</div>
+    <div>
+      {Object.values(Providers).map((provider:Provider, i ) => (
+          <button key={i} title='Sign In' onClick={() => signIn(provider?.id)}>{provider.id}</button>
+        )
+      )}
+    </div>
   )
+ }
 }
 
 export default AuthProviders
--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,5 +1,7 @@
 import { g, auth, config } from '@grafbase/sdk'
 
+
+//@ts-ignore
 const User = g.model('User', {
   name: g.string().length({min:2, max:20}),
   email: g.string().unique(),
@@ -9,7 +11,11 @@ const User = g.model('User', {
   linkedInUrl: g.url().optional(),
   projects: g.relation(() => Project).list().optional(),
 
+}).auth((rules) => {
+  rules.public().read()
 })
+
+//@ts-ignore
 const Project = g.model('Project', {
   title: g.string(),
   description: g.string(),
@@ -19,9 +25,21 @@ const Project = g.model('Project', {
   category: g.string().search(),
   createdBy: g.relation(() => User),
   
+}).auth((rules) => {
+  rules.public().read(),
+  rules.private().create().delete().update()
+})
+
+const jwt = auth.JWT({
+  issuer: 'grafbase',
+  secret: g.env('NEXTAUTH_SECRET'),
 })
 
 export default config({
-  schema: g
+  schema: g,
+  auth:{
+    providers:[jwt],
+    rules: (rules) => rules.private(),
+  }
  
 })
